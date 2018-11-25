6cbba730866740039293c725baaa980c97b5a8e8
BrainStone <yannick@tedworld.de>
2018-11-25 02:11:58 +0100

    Added basic code

---
+++ src/fractionTest/cpp/test_main.cpp
@@ -0,0 +1,13 @@
+//
+// Copyright (c) 2018 Yannick Schinko
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+//
+#include <gtest/gtest.h>
+
+#include "fraction.hpp"
+
+int main( int argc, char **argv ) {
+	::testing::FLAGS_gtest_output = "xml:gtestresults.xml";
+	::testing::InitGoogleTest( &argc, argv );
+	return RUN_ALL_TESTS();
+}
